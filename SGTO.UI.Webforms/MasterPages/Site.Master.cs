3c44fcc124d35d0c7befb7a05ec1f5c6d521933c
Kimblag <kimberly.blandong@gmail.com>
2025-11-13 16:01:13 -0300

    Se eliminan archivos de médicos que ya no se utilizan

--- SGTO.UI.Webforms/MasterPages/Site.Master.cs
+++ SGTO.UI.Webforms/MasterPages/Site.Master.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Net.NetworkInformation;
 using System.Text;
 using System.Web;
 using System.Web.UI;
